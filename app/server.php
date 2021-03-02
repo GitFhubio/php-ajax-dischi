cc47699ad7f99aa14640c04c9f3378e5749071bc
Fabio Petrone <fa610movic90@gmail.com>
2021-03-02 15:41:43 +0100

    push tecnico

--- app/server.php
+++ app/server.php
@@ -8,3 +8,17 @@ require_once __DIR__ . '/../database/database.php';
 
 header('Content-Type: application/json');
 echo json_encode($dischi);
+
+$filter = isset($_GET["filter"]) ? $_GET["filter"] : '';
+
+if( strlen($filter) !== 0){
+    $dischiFiltered =[];
+    foreach($dischi as $disco){
+        if(strpos($disco['genre'] , $filter) !== false ){
+            $dischiFiltered[] = $disco;
+        }
+    }
+    echo json_encode($dischiFiltered);
+} else{
+    echo json_encode($dischi);
+}
--- js/action.js
+++ js/action.js
@@ -1,8 +1,8 @@
 const app = new Vue( {
     el: '#app',
     data: {
-       selected: 'All',
        listCds: [],
+       filter:'',
     },
     mounted () {
         axios.get('http://localhost:8888/php-ajax-dischi/app/server.php')
@@ -13,6 +13,12 @@ const app = new Vue( {
             }
             ,
     methods:{
-
+    filterGenre(){
+       axios.get('http://localhost:8888/php-ajax-dischi/app/server.php?filter='+this.filter)
+       .then( response => {
+           console.log(response.data);
+           this.listCds=response.data;
+         })
+    }
    }
 });
