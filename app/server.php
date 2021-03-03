c87d6a9f04acd64fefcf0f0fe2198184a2cf9b9e
Fabio Petrone <fa610movic90@gmail.com>
2021-03-03 16:01:05 +0100

    refactoring

--- app/server.php
+++ app/server.php
@@ -6,43 +6,36 @@
 
 require_once __DIR__ . '/../database/database.php';
 
-$discgenres=[];
-foreach($dischi as $disco){
-  if(!in_array( $disco['genre'] ,$discgenres )){
-    $discgenres[]=$disco['genre'];
-  }}
-
-// print_r($discgenres);
-
-header('Content-Type: application/json');
-// echo json_encode($dischi);
-
-if (isset($_GET["filter"]) && $_GET["filter"] != 'All') {
-  $filter=$_GET["filter"];
-
-  foreach($dischi as $disco){
-    if(!in_array( $filter ,$discgenres )){
-      http_response_code(400);
+function genres($array){
+  $discgenres=[];
+  foreach($array as $disco){
+    if(!in_array( $disco['genre'] ,$discgenres )){
+      $discgenres[]=$disco['genre'];
     }}
+    return $discgenres;}
+    // print_r($discgenres);
+    header('Content-Type: application/json');
+    // echo json_encode($dischi);
+    if (isset($_GET["filter"]) && $_GET["filter"] != 'All') {
+      $filter=$_GET["filter"];
+      foreach($dischi as $disco){
+        if(!in_array( $filter ,genres($dischi) )){
+          return http_response_code(400);
+        }}
+        // qui avevo messo inizialmente isset al posto di !empty
+        if (!empty($filter) ) {
+          $dischiFiltered =[];
+          foreach($dischi as $disco){
+            if(strpos($disco['genre'] , $filter) !== false ){
+              $dischiFiltered[] = $disco;
+              // pusho il disco nell'array di dischi
+            }
+          }
+          // potevo usare anche l'array filter
+          // $dischiFiltered = array_filter($dischi, function ($disco) use ($filter) {
+          //       return ($disco['genre'] == $filter);});
 
-// qui avevo messo inizialmente isset al posto di !empty
-if (!empty($filter) ) {
-  $dischiFiltered =[];
-    foreach($dischi as $disco){
-        if(strpos($disco['genre'] , $filter) !== false ){
-            $dischiFiltered[] = $disco;
-            // pusho il disco nell'array di dischi
+          echo json_encode($dischiFiltered);
+        }}else{
+          echo json_encode($dischi);
         }
-
-    }
-
-// potevo usare anche l'array filter
-
-    // $dischiFiltered = array_filter($dischi, function ($disco) use ($filter) {
-    //       return ($disco['genre'] == $filter);});
-
-    echo json_encode($dischiFiltered);
-
-}}else{
-    echo json_encode($dischi);
-}
